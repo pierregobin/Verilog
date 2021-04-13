a4874c1eda4137ea10b14e6b3d806bc3c9394bfc
pierregobin <pierregobin@free.fr>
2021-04-13 19:57:38 +0200

    Add files via upload

---
+++ counter/tb_counter.v
@@ -0,0 +1,23 @@
+module test;
+
+  /* Make a regular pulsing clock. */
+  reg clk = 0;
+  wire [7:0] value;
+  reg reset = 0;
+
+  always #5 clk = !clk;
+
+initial begin
+    $display("hello counter");
+    $monitor($time, "clk=%b, value=%b",clk,value);
+       $dumpfile("counter.vcd");
+       $dumpvars(0,c1);
+       $dumpvars(0,test);
+     #17 reset = 1;
+     #11 reset = 0;
+     #29 reset = 1;
+     #11 reset = 0;
+     #1000 $finish;
+  end
+  counter c1 (value, clk, reset);
+endmodule // test
